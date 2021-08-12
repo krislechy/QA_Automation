7ce3fd7690edbe91a69ed9741487540fbaa3bf87
krislechy <bogdanpjatko@rocketmail.com>
2021-08-12 22:46:05 +0300

    up

---
+++ SeleniumApp/SeleniumApp/Program.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+
+namespace SeleniumApp
+{
+    class Program
+    {
+        enum LanguageType
+        {
+            Russian=0,
+            English=1,
+        }
+        class Vacancies
+        {
+            public string header { get; set; }
+            public string body { get; set; }
+            public string footer { get; set; }
+        }
+        static string link = "https://careers.veeam.ru/vacancies";
+        static OpenQA.Selenium.Chrome.ChromeDriver chrome;
+        static IJavaScriptExecutor executor;
+        static void Main(string[] args)
+        {
+            InitBrowser();
+            var result = FindCountVacancies(LanguageType.Russian);
+            if (result != null)
+            {
+                Console.WriteLine("Количество найденных вакансий {0}:", result.Count());
+                int i = 1;
+                foreach (var s in result)
+                {
+                    Console.WriteLine("{3}:{0}\n{1}\n{2}", s.header, s.body, s.footer, i);
+                    i++;
+                }
+            }
+            Console.ReadKey();
+        }
+        static void InitBrowser()
+        {
+            chrome = new OpenQA.Selenium.Chrome.ChromeDriver();
+            chrome.Navigate().GoToUrl(link);
+            chrome.Manage().Window.FullScreen();
+        }
+        static List<Vacancies> FindCountVacancies(LanguageType language)
+        {
+            ScriptClick(FindButtonByInnerText("Все отделы"));
+            var links_of_departments = chrome.FindElementsByTagName("a");
+            var departments = links_of_departments.Where(x => x.GetAttribute("class") == "dropdown-item");
+            Dictionary<int, string> dictionary = new Dictionary<int, string>();
+            int counter = 1;
+            Console.WriteLine("Выберите интересующий отдел:");
+            foreach (var s in departments)
+            {
+                dictionary.Add(counter, s.Text);
+                counter++;
+                Console.WriteLine("{0}:{1}", counter, s.Text);
+            }
+            var selected = Console.ReadLine();
+            int _case = 0;
+            while (true)
+            {
+                if (Int32.TryParse(selected, out _case) == true)
+                {
+                    if (_case >= dictionary.Count() || _case < 1)
+                    {
+                        Console.WriteLine("Неверный номер, выберите другой");
+                        selected = Console.ReadLine();
+                    }
+                    else break;
+                }
+            }
+            var department_link = chrome.FindElementByLinkText(dictionary[_case]);
+            ScriptClick(department_link);
+            var buttons_in_dropdown = chrome.FindElementsByTagName("button");
+            if (buttons_in_dropdown != null && buttons_in_dropdown.Count() > 0)
+            {
+                var button_language = buttons_in_dropdown.Where(x => x.Text == "Все языки").First();
+                ScriptClick(button_language);
+            }
+            var input_language = chrome.FindElementsByTagName("div").Where(x => x.GetAttribute("class") == "show dropdown").First();
+            string str_language = "";
+            switch (language)
+            {
+                case (LanguageType.Russian):
+                    {
+                        str_language = "lang-option-0";
+                        break;
+                    }
+                case (LanguageType.English):
+                    {
+                        str_language = "lang-option-1";
+                        break;
+                    }
+            }
+
+            var checkboxs = input_language.FindElements(By.TagName("input")).Where(x => x.GetAttribute("id") == str_language);
+            if (checkboxs != null && checkboxs.Count() > 0)
+            {
+                var englanguage = checkboxs.First();
+                ScriptClick(englanguage);
+            }
+            var tt = chrome.FindElementsByTagName("a").Where(x => x.GetAttribute("class") == "card card-no-hover card-sm");
+            var result = new List<Vacancies>();
+            foreach (var s in tt)
+            {
+                string header = "", body = "", footer = "";
+                var div = s.FindElements(By.TagName("div"));
+                foreach (var t in div)
+                {
+                    var classname = t.GetAttribute("class");
+                    if (classname.StartsWith("card-header"))
+                    {
+                        header = t.Text;
+                    }
+                    else if (classname.StartsWith("card-body"))
+                    {
+                        body = t.Text;
+                    }
+                    else if (classname.StartsWith("card-footer"))
+                    {
+                        footer = t.Text;
+                    }
+                }
+                result.Add(new Vacancies() { header = header, body = body, footer = footer });
+            }
+            return result;
+        }
+        static void ScriptClick(IWebElement element)
+        {
+            if (element == null)
+                return;
+            if (executor == null)
+                executor = chrome as IJavaScriptExecutor;
+            executor.ExecuteScript("arguments[0].click();", element);
+        }
+        static IWebElement FindButtonByInnerText(string text)
+        {
+            var buttons = chrome.FindElementsByTagName("button");
+            if (buttons != null && buttons.Count() > 0)
+            {
+                var button = buttons.Where(x => x.Text == text);
+                if (button != null && button.Count() > 0)
+                {
+                    return button.First();
+                }
+            }
+            return null;
+        }
+    }
+}
---
+++ TaskKiller/TaskKiller/Program.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Timers;
+
+namespace TaskKiller
+{
+    class Program
+    {
+        static System.Timers.Timer timer;
+        static int Allowed_Time;
+        static int Interval;
+        static string Name;
+        static DateTime dt { get { return DateTime.Now; } }
+        static void Main(string[] args)
+        {
+            if (args.Length == 3)
+            {
+                if (!String.IsNullOrEmpty(args[0]))
+                {
+                    Name = args[0];
+                    bool is_time = Int32.TryParse(args[1], out Allowed_Time);
+                    if (is_time && Allowed_Time >= 0)
+                    {
+                        bool is_interval = Int32.TryParse(args[2], out Interval);
+                        if (is_interval && Interval > 0)
+                        {
+                            StartTimer();
+                        }
+                        else Console.WriteLine("Значение параметра \"{0}\" не является натуральным числом или он меньше или равняется 0.", args[2]);
+                    }
+                    else Console.WriteLine("Значение параметра \"{0}\" не является натуральным числом или меньше 0.", args[1]);
+                }
+                else Console.WriteLine("Значение параметра \"{0}\" неверное.", args[0]);
+            }
+            else Console.WriteLine("Неправильны параметры, входные параметры: taskkiller <Имя процесса> <Время жизни> <Период>.\nПример: notepad 5 1.");
+            Console.ReadKey();
+        }
+        private static void StartTimer()
+        {
+            var interval_minutes = TimeSpan.FromMinutes(Interval);
+            timer = new System.Timers.Timer() { Enabled = true, Interval = interval_minutes.TotalMilliseconds };
+            timer.Elapsed += Timer_Elapsed;
+            timer.Start();
+            Console.WriteLine("{0:HH:mm:ss} Таймер запущен для поиска процесса: \"{1}\", проверка каждые {2}.", dt, Name, interval_minutes);
+        }
+
+        private static void StopTimer()
+        {
+            timer.Stop();
+            timer.Dispose();
+        }
+
+        private static void Timer_Elapsed(object sender, ElapsedEventArgs e)
+        {
+            Console.WriteLine("{0:HH:mm:ss} Попытка найти процесс \"{1}\"", e.SignalTime, Name);
+            KillProcesses();
+        }
+
+        private static void KillProcesses()
+        {
+            var proc = Process.GetProcesses();
+            bool IsFound = false;
+            foreach (Process s in proc)
+            {
+                if (s.ProcessName.Equals(Name, StringComparison.OrdinalIgnoreCase))
+                {
+                    DateTime start_dt = s.StartTime;
+                    var different = (DateTime.Now - start_dt);
+                    if (different.TotalMinutes > Allowed_Time)
+                    {
+                        try
+                        {
+                            s.Kill();
+                            Console.WriteLine("{0:HH:mm:ss} ИД процесса: {1} с именем: \"{2}\" был уничтожен.", dt, s.Id, Name);
+                            IsFound = true;
+                        }
+                        catch (Exception ex)
+                        {
+                            Console.WriteLine(ex.Message);
+                        }
+                    }
+                    else
+                    {
+                        IsFound = true;
+                        var elapsed = TimeSpan.FromMinutes(Allowed_Time) - different;
+                        Console.WriteLine("{0:HH:mm:ss} Процесс с ИД: {1} и именем: {2} работает уже: {3} осталось: {4}", dt, s.Id, Name, different, elapsed);
+                    }
+                }
+            }
+            if (!IsFound)
+                Console.WriteLine("{0:HH:mm:ss} Процесс с именем \"{1}\" не найден.", dt, Name);
+        }
+    }
+}
