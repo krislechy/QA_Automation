fd16e77c2ebe0528ab5ae92cb800795afb95d44e
krislechy <bogdanpjatko@rocketmail.com>
2021-08-18 20:38:56 +0300

    Fixed bugs
    Renamed Class and add using class "Page" for avaiable dispose methods

---
+++ SeleniumApp/SeleniumApp/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Interactions;
+using OpenQA.Selenium.Internal;
+//using OpenQA.Selenium.Support.PageObjects;
+using OpenQA.Selenium.Support.UI;
+using SeleniumExtras.PageObjects;
+
+namespace SeleniumApp
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            using (Page page = new Page())
+            {
+                page.InitBrowser(TypeDriver.Chrome, true, TimeSpan.FromSeconds(10));
+                page.GoToUrl("https://careers.veeam.ru/vacancies", true);
+                var ListOfVacancies = page.FindCountVacancies(TypeLanguage.Russian, "Разработка продуктов");
+                var result = page.ResultVacancies(ListOfVacancies);
+                Console.WriteLine($"Количество найденных вакансий {result}");
+            }
+            Console.ReadKey();
+        }
+    }
+}
